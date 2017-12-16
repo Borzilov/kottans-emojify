647bd1e1c1c0642c24e49e046e9fb3d7d437a49b
sudodoki <smd.deluzion@gmail.com>
2017-12-16 15:42:32 +0200

    Upto images + onemojify

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,15 @@ import { Redirect } from 'react-router-dom';
 
 import API from '../services/instagram-api';
 
+const EPhoto = ({ url, onEmojify }) => {
+  return (
+    <p>
+      <img src={url} />
+      <button onClick={() => onEmojify(url)}>Emojify</button>
+    </p>
+  );
+};
+
 class Home extends Component {
   constructor() {
     super();
@@ -14,8 +23,35 @@ class Home extends Component {
       data: []
     };
   }
+  componentWillMount() {
+    this.setState({ isLoading: true });
+    API.getPhotos().then(
+      data => this.setState({ data, error: null, isLoading: false }),
+      error => this.setState({ data: [], error, isLoading: false })
+    );
+  }
+  emojify(url) {
+    console.log(`Emojyfying ${url}`);
+  }
   renderHome() {
-    return <h1>THIS IS HOME</h1>;
+    const { isLoading, error, data } = this.state;
+    if (isLoading) {
+      return <span>Loading...</span>;
+    }
+    return error ? (
+      <span>Sorry, error: {error}</span>
+    ) : (
+      <ul>
+        {data.map(photoObjs => {
+          const id = photoObjs.id;
+          return photoObjs.urls.map((url, index) => (
+            <li key={`${id}-${index}`}>
+              <EPhoto onEmojify={this.emojify} url={url} />
+            </li>
+          ));
+        })}
+      </ul>
+    );
   }
   render() {
     return this.state.hasAuth ? this.renderHome() : <Redirect to="/login" />;
